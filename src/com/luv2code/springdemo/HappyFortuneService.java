f42a6c839f2ee2d1f33f2c590c7c85312fe4609d
maahzaade <mahdiye.nikraftar@gmail.com>
2018-12-09 12:48:56 -0500

    constructor dependency injection

---
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortuneService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return "Today is your lucky day!";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
+    }
+}
--- src/com/luv2code/springdemo/PingPongCoach.java
+++ src/com/luv2code/springdemo/PingPongCoach.java
@@ -1,18 +1,33 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PingPongCoach implements Coach {
 
+	private FortuneService fortuneService;
+
 	// define a default constructor
 	public PingPongCoach() {
-		System.out.println(">> PingPongCoach: inside default constructor");
+		System.out.println("Inside default constructor: PingPongCoach");
 	}
-		
+
+	@Autowired
+	public PingPongCoach(FortuneService fortuneService) {
+		System.out.println("Inside constructor: PingPongCoach(FortuneService fortuneService)");
+		this.fortuneService = fortuneService;
+
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your pingpong drop shot";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
